329b14109449e1f4f6092082718058186ff7b7c7
Gavin <gavinferdinandcheezhukaiwen+GitHub@gmail.com>
2023-10-15 12:50:47 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
 
               <div className="flex items-center self-start mt-2 lg:self-center">
-                <Link href="" // Input gdrive resume here as url https://docs.google.com/document/d/xxxxxxxxx/edit?usp=sharing
+                <Link href="https://drive.google.com/file/d/1kA4E8p-B9PX8lO0sx_tbZR5Teds1RimK/view?usp=sharing" // Input gdrive resume here as url https://docs.google.com/document/d/xxxxxxxxx/edit?usp=sharing
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                   md:p-2 md:px-4 md:text-base
